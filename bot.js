d200c8ae46d57832b6792b7f4c698d5030f96c07
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-11-24 18:50:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -659,7 +659,7 @@ client.on('message', async msg => {
 	if (!msg.content.startsWith(prefix)) return undefined;
 	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
-	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+	const url = args[1] ? args[1].replace(/<(.+)>/g, '!') : '';
 	const serverQueue = queue.get(msg.guild.id);
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(prefix.length)
